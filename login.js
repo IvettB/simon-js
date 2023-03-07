1f51a51bf56245792441db39aba7b08a76a9b932
IvettB <77896065+IvettB@users.noreply.github.com>
2023-03-06 17:58:21 -0700

    Rewriting login.js

--- login.js
+++ login.js
@@ -1,5 +1,11 @@
+//function login() {
+ // const nameEl = document.querySelector("#name");
+  //localStorage.setItem("userName", nameEl.value);
+ // window.location.href = "play.html";
+//}
+
 function login() {
-  const nameEl = document.querySelector("#name");
+  const nameEl = document.querySelector('#name');
   localStorage.setItem("userName", nameEl.value);
   window.location.href = "play.html";
 }
