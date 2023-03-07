dc44b187f078faa7f9d25ca95de9ba9b96b1425a
IvettB <77896065+IvettB@users.noreply.github.com>
2023-03-06 18:08:09 -0700

    Final push, rewriting updateScores

--- play.js
+++ play.js
@@ -176,6 +176,30 @@ class Game {
     localStorage.setItem('scores', JSON.stringify(scores));
   }
 
+  /*updateScores(userName, score, scores) {
+    const date = new Date().toLocaleDateString();
+    const newScore = { name: userName, score: score, date: date };
+
+    let found = false;
+    for (const [i, prevScore] of scores.entries()) {
+      if (score > prevScore.score) {
+        scores.splice(i, 0, newScore);
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      scores.push(newScore);
+    }
+
+    if (scores.length > 10) {
+      scores.length = 10;
+    }
+
+    return scores;
+  }*/
+
   updateScores(userName, score, scores) {
     const date = new Date().toLocaleDateString();
     const newScore = { name: userName, score: score, date: date };
